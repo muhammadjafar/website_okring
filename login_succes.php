48a23bb085ce4190cbda56875f88ffb0b43d9cda
DESKTOP-OAJEQPT\USER <wahyuputri1476@gmail.com>
2017-05-16 16:08:05 +0700

    no message

---
+++ login_succes.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<h2>Login Berhasil</h2>
+<p>Selamat datang
+<?php echo $model->user ?><p>
+<p>[<a href="http://localhost/CI/index.php/Login/logout">Logout</a>]</p>
+</body>
+</html>
