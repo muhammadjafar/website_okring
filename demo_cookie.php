a9bebe3609a9cccf9b941a208b1489b48cb7f64c
DESKTOP-OAJEQPT\USER <wahyuputri1476@gmail.com>
2017-05-16 16:00:34 +0700

    session cookie

---
+++ demo_cookie.php
@@ -0,0 +1,14 @@
+<?php
+class demo_cookie extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->helper('cookie');
+	}
+	public function index(){
+		set_cookie('myvar',100,60000*30);
+		$this->load->view('cookie_view');
+	}
+	}
+	?>
+	
+	
