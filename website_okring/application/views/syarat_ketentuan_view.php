f8e29b475fe5b11c630830e1a36c01bd2c5e0cdb
RIZKIYAH\RICHI <riski.richi@gmail.com>
2017-05-26 08:47:55 +0700

    Syarat dan Ketentuan Penjual

---
+++ website_okring/application/views/syarat_ketentuan_view.php
@@ -0,0 +1,117 @@
+<html>
+<head>
+<title>Syarat dan Ketentuan Sebagai Penjual Catering</title>
+</head>
+<body>
+<h1><center>3 LANGKAH MUDAH JUALAN</center></h1></br>
+
+<ol>	
+	<b><li>JUAL</br></br></li></b>
+	Temukan banyak pembeli dan dapatkan penghasilan yang lebih besar dengan berjualan online di Okring</br></br>
+
+	<ol type="A">
+		<li>Buka Depot</br></li>
+		Apakah anda ingin peluang bisnis yang lebih besar di dunia Online? 
+		Apakah Anda ingin mengembangkan bisnis offline Anda ke dunis Online? 
+		Anda  mengunjungi website yang tepat!</br></br>
+
+		Dengan membuka Depot online di Okring Anda akan otomatis terhubung dengan banyak pembeli. Proses membuka depot pun sangatlah mudah. 
+		Untuk membuka depot cukup dengan mendaftarkan akun sebagai pembeli di website okring. Isi semua data yang dibutuhkan</br></br>
+
+		<li>Tambahkan produk</br></li>
+		Setelah akun anda terdaftar sebagai penjual, sekarang saatnya anda memasukkan informasi produk-produk yang ingin anda jual. 
+		lalu klik icon Tambah Produk.Isi selengkap mungkin mengenai produk yang akan anda jual, seperti nama produk,deskripsi produk,berat produk,
+		harga produk, foto produk dst. setelah selesai, kik tombol simpan.</br></br>
+
+		<li>Terima Pesanan</br></li>
+			<ul type="square">
+				<li>Anda akan mendapat notifikasi jika mendapat order baru.Untuk memeriksa order baru tersebut, pilih menu "Notifikasi".
+				Lalu klik "Pesanan Baru". </li>
+
+				<li>Di Halaman ini, Anda bisa melihat detail pesanan Anda terima dengan klik menu "Detail Transaksi".
+				untuk menerima pesanan, pilih " Terima Pesanan".</li>
+
+				<li>Note : jika karena suatu hal Anda tidak bisa memenuhi pesanan, pilih "Tolak Pesanan" dan isi alasan yang sesuai pada "Alasan Penolakan". </br></br></li>
+				</br>
+			</ul>
+	</ol>
+	
+	<b><li>KIRIM</br></li></b></br>
+		<ol type="A">
+			<li>KIRIM BARANG</br></li>
+			Kirim Barang Pesanan Pembeli. Bungkus barang sebaik mungkin dan antarkan ke Alamat tujuan yang sudah ditentukan bersama.</br></br>
+
+			<li>KONFIRMASI PENGIRIMAN</br></br></li>
+				<ul type="square">
+					<li>Klik Konfirmasi Pengiriman. </br></li>
+					Setelah melakukan pengiriman, kini Anda harus melakukan konfirmasi pengiriman di website Okring. Pilih menu "Notifikasi". Lalu klik "Konfirmasi Pengiriman"</br></br>
+
+					<li>Pilih salah satu pengiriman</br></li>
+					Pilih order yang sudah Anda kirimkan, lalu pilih klik tombol "Detil Transaksi".</br></br>
+
+					<li>Klik Ya untuk konfirmasi</br></li>
+					Masukkan tanggal pengiriman kemudian klik tombol "Konfirmasi".</br></br>
+				</ul>
+			<li>PANTAU PENGIRIMAN</li></br>
+				<ul type="square">
+					<li>Klik Status Pengiriman</br></li>
+					Setelah Anda sebagai penjual melakukan konfirmasi pengiriman barang, Pilih menu "Notifikasi". Lalu klik "Status Pengiriman" untuk melihat status pengiriman Anda.
+					</br></br>
+					<li>Klik Lacak</br></li>
+					Klik tombol "Lacak" pada pesanan yang ingin Anda ketahui status pengirimannya. Telah dikirim atau belum.</br></br>
+				</ul>
+		</ol>
+
+
+
+<br><b><li>TERIMA UANG</h2></li></b></br>
+		<ol type="A">
+			<li>TUNGGU KONFIRMASI PEMBELI</li></br>
+
+			Konfirmasi Pembeli</br>
+			Jika pembeli sudah melakukan konfirmasi terima barang, Anda akan mendapatkan notifikasi (notifikasi melalui email dan notifikasi di website Okring).</br>
+
+			Catatan : Khawatir pembeli tidak melakukan konfirmasi terima barang? Tidak perlu takut, jika pembeli tidak melakukan konfirmasi penerimaan barang hingga 3 hari setelah pihak Pengantar menyatakan pengiriman sudah tiba di tujuan, order akan diselesaikan secara otomatis oleh sistem Okring.</br>
+			</br>
+
+			<li>TERIMA PENILAIAN</li></br>
+				<ul type="square">
+					<li>Klik Ulasan</li>
+					Pembeli yang membeli produk dari toko kamu berhak untuk memberikan ulasan terhadap produk dan tingkat pelayanan yang kamu berikan kepada pembeli.
+					</br>
+					Untuk melihat ulasan produk kamu, pilih menu Notifikasi. Lalu klik Review.</br></br>
+
+					Tips : Ulasan sangat penting karena bisa mempengaruhi performa depot Anda di Okring.</br>
+					Baca selengkapnya mengenai ulasan di sini.</br></br>
+
+					<li>Balas Ulasan Pembeli</li>
+					Sebagai penjual, kamu juga bisa memberikan nilai kepada pembeli dan membalas tanggapan dari pembeli. Klik "Berikan Komentar" pada ulasan yang ingin kamu tanggapi.
+					</br></br>
+					<li>Balas Ulasan Pembeli</li>
+					Pilih penilaian kamu terhadap pembeli dengan memilih salah satu dari tiga icon smile yang memiliki nilai  negatif,  netral, dan  positif.akhiri dengan klik "Kirim".
+					</br></br>
+					<li>Balas Ulasan Pembeli</li>
+					Tulis tanggapan kamu terhadap penilaian pembeli, kemudian klik "Kirim".</br></br>
+				</ul>
+			<li>TARIK DANA</li></br>
+				<ul type="square">
+					<li>Lihat Saldo</li>
+					Anda sudah menyelesaikan order pertama Anda? Selamat!</br></br>
+
+					Okring akan memasukkan uang hasil penjualan Anda ke Saldo Okring milik Anda. Untuk menarik dana dari Saldo Okring ke rekening bank milik Anda, pilih menu "Notifikasi". Lalu klik pilihan "Tarik Dana".
+					</br></br>
+					
+					<li>Klik Tarik Dana</li>
+					Di halaman Detil Saldo, Anda bisa memeriksa alur dana yang masuk dan keluar dari Saldo Okring Anda.</br>
+
+					</br>Untuk mendapatkan uang hasil penjualan Anda, klik tombol "Tarik Dana".
+
+					Isi detail penarikan dana</br>
+					Isi nominal saldo dan nomor rekening yang dituju. Setelah itu klik tombol "Tarik Dana".</br></br>
+
+					Pihak Okring akan kemudian memproses permintaan penarikan dana ini dan akan memberikan notifikasi jika proses pencairan dana sudah selesai.
+					</br>
+				</ul>
+</ol>
+</body>
+</html>
