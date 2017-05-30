a9d8742a32940399088e6d3488dc4c62e8f0e74d
RIZKIYAH\RICHI <riski.richi@gmail.com>
2017-05-30 15:30:51 +0700

    edit profil, tambah barang, list barang, delete barang

---
+++ website_okring/application/views/penjual/footer.php
@@ -0,0 +1,12 @@
+</section><!-- /.content -->
+                <div class="footer-main">
+                    Copyright &copy <a href="<?php echo base_url()?>/index.php">O Kring</a>, 2017
+                </div>
+            </aside><!-- /.right-side -->
+
+        </div><!-- ./wrapper -->
+
+
+
+</body>
+</html>
